fd79aeb8aa37ecc5cabccd4918529bdf0ef2ec44
gyanendra singh <gyanendra08thakur@gmail.com>
2025-09-10 02:45:40 +0530

    updated

--- function.js
+++ function.js
@@ -117,28 +117,165 @@ function getArray(givearray){
 
 console.log(getArray(arr))
 
+//scope
+
+let variable= 223;  // there variBLE have their scope in entire code
+const variable2= 299;
+var variable3= 8899;
+
+console.log(variable);
+console.log(variable2);
+console.log(variable3);
+
+function varr(){                         
+
+   let variable=11;                    // {} in object can decleareation and remaing {} with fun,if,else etc called scope
+
+   const variable2=22;
+   var variable3= 33;            //can access these let const only inside {}
+                                //scope in browser and code app is totally different
 
 
 
 
+}
+//[ note:- var break the scope of {} thats why we dont use it]
+
+
+console.log(variable);
+console.log(variable2);
+console.log(variable3) ;
+
+//nested scope
+
+function name(){                    
+
+const username ="shiva"
+
+ function contact() {
+
+   email= "gyanendrasingh";          // scope------->name cant access the contact but contact cam the name
+   id= 223344;                       // child can acces the parents scope but parent cannot access the child scope
+   website= "free_pani.com"
+   console.log(`this is username${username}, this is email ${email}, and the id is ${id}`) //username can be access
+
+ }
+
+//console.log(` username is ${username}, and his website is ${ website}.`)  //website cannot acess 
+console.log(username);
+   contact();
+}
+
+name()
+
+ if(true){
+
+   const name = "father";
+   
+     if ( name === "father"){                 // child can acces the parents scope but parent cannot access the child scope
+
+      const website = "www.globalscope.com";
+      console.log(name + website);
+      
+     }
+
+    // console.log(webite)
+
+}
+// console.log(name)
+
+
+//***********************************intresting*********************************** */
+
+
+// hosting 
+
+ console.log(Addation(33)) //output obtain           // hosting before declearation is allowed  [not stored in var]
+
+function Addation(num1){
+return num1*99
+
+}
+
+
+//   console.log(sum(33))                     // hosting before decleation is not allowed          [stored in var]
+let thesum = function sum( num1){                  //this is expression ( function stored in Variable )
+                                                  // variable in js can store almost everything  like function object json etc
+
+return num1+22
+
+}
+
+
+//arrow function and this
+
+
+const data ={
+   userName: "gyanendra",
+price : 9999,
+
+  welcomeMessage: function(){
+   console.log(`welcome to website ${this.userName}`); // "this" allow to access all the element in this scope
+
+   console.log(this);
+   
+  }
+}
+
+data.welcomeMessage()
+
+
+data.userName ="shiva"     //value change
+data.welcomeMessage()
+console.log(this.price);  //undefine , as the scope of this is not define
+
+console.log(this);       // no global object thats why it is empty
+
+function chai(){
+   username= "gyanendra"
+   console.log(this)          //give some output in function whhen declesred
+   console.log(this.userName)   //undefine as this function run with object only
+}
+chai()
+
+//arrow function
+
+//syntex
+const coffee= () => {
+  let name= "abhay";
+   console.log(this.name);         // "undefine"  
+   console.log(this)              // "{}"  ----> ouput
+}
+
+coffee()
+
+
+const summ= (num1, num2) => {            // also called explicit return
+return num1+num2;    
+}                   
+console.log(summ(666, 558))
+
+
+//implicit return
+const addTwo= (num1, num2) => num1+num2               // no need to put {} and return , used for smaller/ single line function
+const addthree= (num1, num2, num3) =>   ( num1+num2+num3)       //can use () to wrap the function, if use {} then return keyword is necessry in function 
 
 
 
+//IFFE   immediately invoked function
+//for immidiate execution of function to resist from global scope pallution
 
+//syntex
 
+( function chaii() {                           //also called 
+   console.log(`DB one is connected `);
+   
+})();                                      // dont forget to use "; "  it will produce error, ";" use to end the function
 
-
-
-
-
-
-
-
-
-
-
-
-
+( ( nameone) => {
+   console.log(`DB two is connected ${nameone} `);
+   
+})("shiva singh");
 
 
 
