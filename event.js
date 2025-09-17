58ffbc3e7dd4bf89cd3e605544032cfd191537cb
gyanendra singh <gyanendra08thakur@gmail.com>
2025-09-17 17:08:51 +0530

    complete

--- event.js
+++ event.js
@@ -27,6 +27,89 @@ console.log("events");
 //current target
 //client x, client y, tilt x, tilt y, screen x, screen y
 // alt key, control key, shiftkey, keycode
-// bubbling up
-// capturing up
+// bubbling up     ( child elemnt to parent element)
+// capturing up   ( parent elemnt to child element)
 //stop propogation
+
+//right way to create the event function
+function nameupdate() {let head= document.querySelector("#heading3")
+head.textContent= " hello shiva";}
+
+let head= document.querySelector("#heading3")
+head.addEventListener('mouseover', nameupdate);  // dont give execution , give only refrence of the function
+
+head.removeEventListener('mouseover', nameupdate);
+
+
+// addElement phase applyng to bubbling phase by defalult
+// 
+
+//event object
+
+
+
+
+// changing the default behaviour of element like changing the achor tag to prevent the the link openinng
+
+
+  let anchrele= document.querySelector('#link');
+  anchrele.addEventListener('click', 
+    function(event) {
+      event.preventDefault();
+      anchrele.textContent ='you clicked me'
+    }
+  )
+
+
+
+
+  //target property  
+  //( can ignore this bucky code and follow the below one)
+/*let divs = document.querySelector('div') 
+function alertdiv(event) {
+  alert('you have clicked on div: ')
+}
+
+for(let  i =0 ; i< divs.length; i++) {
+   let div = divs[i];
+   console.log("event listner added");
+   
+   div.addEventListener('click', alertdiv);
+}  */
+
+
+
+// avoid too many listner
+
+function heading(event) {
+  if (event.target.nodename==='heading') {
+   alert( ' you have clicked on heading :'+ event.target.textContent)
+  }
+}
+let  mydiv = document.querySelector('.container');
+document.addEventListener('click', heading);
+
+// learn about dom content loaded
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
